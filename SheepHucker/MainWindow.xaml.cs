5d391b91106547de31dd6e138681d0db08006f00
anton.shcherbyna <anton.shcherbyna@sigma.software>
2017-11-13 19:53:52 +0200

    init

---
+++ SheepHucker/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+namespace SheepHucker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [DllImport("user32.dll")]
+        public static extern bool PostMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        public const int WM_COMMAND = 0x0112;      // Code for Windows command
+        public const int WM_CLOSE = 0xF060;		 // Command code for close window
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
