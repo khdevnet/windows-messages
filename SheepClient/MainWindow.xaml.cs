bc49cbff133150b245b38345a65dda43b10766e1
anton.shcherbyna <anton.shcherbyna@sigma.software>
2017-11-13 21:41:24 +0200

    add working copy

--- SheepClient/MainWindow.xaml.cs
+++ SheepClient/MainWindow.xaml.cs
@@ -1,17 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SheepClient
 {
@@ -20,9 +15,65 @@ namespace SheepClient
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const int WM_MOUSEMOVE = 0x0200;
+
         public MainWindow()
         {
             InitializeComponent();
+            var appSettings = ConfigurationManager.AppSettings;
+            SheepPlayer.Source = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + appSettings.Get("playerAvatar")));
+            //this.Background = new ImageBrush(new BitmapImage(new Uri(@"pack://application:,,,/myapp;component/Images/icon.png")));
+        }
+
+        private void Window_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            HwndSource.FromHwnd(new WindowInteropHelper(this).Handle)?.AddHook(this.WndProc);
+        }
+
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            switch (msg)
+            {
+                case WM_MOUSEMOVE:
+                    int x = lParam.ToInt32() & 0x0000FFFF;
+                    int y = (int)((lParam.ToInt32() & 0xFFFF0000) >> 16);
+                    test.Content = x + " -" + y;
+                    var newLeft = rnd.Next(Convert.ToInt32(cnv.ActualWidth - SheepPlayer.ActualWidth));
+                    var newTop = rnd.Next(Convert.ToInt32(cnv.ActualHeight - SheepPlayer.ActualHeight));
+                    MoveSheep(x, y);
+
+                    // MouseMove?.Invoke();
+                    break;
+            }
+
+            return IntPtr.Zero;
+        }
+
+        Random rnd = new Random();
+
+        private void MoveSheep(int x, int y)
+        {
+            //Create the animations for left and top
+            DoubleAnimation animLeft = new DoubleAnimation(Canvas.GetLeft(SheepPlayer), x, new Duration(TimeSpan.FromSeconds(1)));
+            DoubleAnimation animTop = new DoubleAnimation(Canvas.GetTop(SheepPlayer), y, new Duration(TimeSpan.FromSeconds(1)));
+
+            //Set an easing function so the button will quickly move away, then slow down
+            //as it reaches its destination.
+            animLeft.EasingFunction = new CubicEase() { EasingMode = EasingMode.EaseOut };
+            animTop.EasingFunction = new CubicEase() { EasingMode = EasingMode.EaseOut };
+
+            //Start the animation.
+            SheepPlayer.BeginAnimation(Canvas.LeftProperty, animLeft, HandoffBehavior.SnapshotAndReplace);
+            SheepPlayer.BeginAnimation(Canvas.TopProperty, animTop, HandoffBehavior.SnapshotAndReplace);
+        }
+
+        private void MainWindow_OnMouseMoveMouseMove(object sender, MouseEventArgs e)
+        {
+            var relativePosition = e.GetPosition(this);
+            var point = PointToScreen(relativePosition);
+
+            // _x.VerticalOffset = point.Y;
+
         }
     }
 }
--- SheepHucker/MainWindow.xaml.cs
+++ SheepHucker/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Input;
 
 namespace SheepHucker
 {
@@ -10,15 +12,46 @@ namespace SheepHucker
     public partial class MainWindow : Window
     {
         [DllImport("user32.dll")]
-        public static extern bool PostMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        public static extern bool PostMessage(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
 
         public const int WM_COMMAND = 0x0112;      // Code for Windows command
         public const int WM_CLOSE = 0xF060;		 // Command code for close window
+        public const int WM_MOUSEMOVE = 0x0200;
 
         public MainWindow()
         {
             InitializeComponent();
 
+
+        }
+
+        private void MainWindow_OnMouseMoveMouseMove(object sender, MouseEventArgs e)
+        {
+            var relativePosition = e.GetPosition(this);
+            var point = PointToScreen(relativePosition); 
+
+            SendMousePosition("SheepClient", WM_MOUSEMOVE, (IntPtr)0, MakeLParam((int)relativePosition.X, (int)relativePosition.Y));
+            // test.Content = point.X + " - " + point.Y;
+        }
+
+        public static IntPtr MakeLParam(int wLow, int wHigh)
+        {
+            return (IntPtr)(((short)wHigh << 16) | (wLow & 0xffff));
+        }
+
+        private void SendMousePosition(string hWnd, int Msg, IntPtr wParam, IntPtr lParam)
+        {
+            try
+            {
+                IntPtr hWndNotepad = Process.GetProcessesByName(hWnd)[0].MainWindowHandle;
+                if (hWndNotepad != null)
+                {
+                    PostMessage(hWndNotepad, Msg, wParam, lParam);
+                }
+            }
+            catch (Exception e)
+            {
+            }
         }
     }
 }
